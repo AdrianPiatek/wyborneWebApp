0383ce6c422685675af36fee409d84d845d9e581
Maciek <Your_Majesty@wp.pl>
2021-12-27 11:56:48 +0100

    added price to dish class

--- src/main/java/com/example/wyborne/objects/Dish.java
+++ src/main/java/com/example/wyborne/objects/Dish.java
@@ -27,11 +27,14 @@ public class Dish {
 
     private Long id;
     private String name;
+    private Integer price;
     private String description;
 
 
-    public Dish(String name, String description) {
+
+    public Dish(String name, Integer price, String description) {
         this.name = name;
+        this.price = price;
         this.description = description;
     }
 
