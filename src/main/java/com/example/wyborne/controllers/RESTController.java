1291f4df4606531c208f32ace0f6388c128bbae6
Maciek <Your_Majesty@wp.pl>
2021-12-27 12:42:19 +0100

    adding RESTController

---
+++ src/main/java/com/example/wyborne/controllers/RESTController.java
@@ -0,0 +1,14 @@
+package com.example.wyborne.controllers;
+
+import com.example.wyborne.objects.Dish;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@AllArgsConstructor
+public class RESTController {
+
+}
