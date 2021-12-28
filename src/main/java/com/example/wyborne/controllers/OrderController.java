63270a4b8b9fe20097502dc36c8d8970ad2d7824
Maciek <Your_Majesty@wp.pl>
2021-12-28 19:01:35 +0100

    sending emails

--- src/main/java/com/example/wyborne/controllers/OrderController.java
+++ src/main/java/com/example/wyborne/controllers/OrderController.java
@@ -1,13 +1,10 @@
 package com.example.wyborne.controllers;
 
-import com.example.wyborne.objects.Order;
 import com.example.wyborne.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 
 @RestController
@@ -16,9 +13,15 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/order")
-    public void order(@RequestBody Order order) {
-        orderService.showAll(order);
+    @PostMapping("/order2")
+    public void showAll(@RequestBody Map<String, ?> map) {
+
+        orderService.showAll(map);
     }
 
+    @PostMapping("/order")
+    public void sendMail(@RequestBody Map<String, ?> map) {
+
+        orderService.sendMail(map);
+    }
 }
--- src/main/java/com/example/wyborne/services/OrderService.java
+++ src/main/java/com/example/wyborne/services/OrderService.java
@@ -1,22 +1,80 @@
 package com.example.wyborne.services;
 
-import com.example.wyborne.objects.Order;
 import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 
 @Service
 @AllArgsConstructor
 public class OrderService {
 
-    public void showAll(Order order) {
-        System.out.println("\n\n\n");
-        System.out.println(order.getName());
-        System.out.println(order.getEmail());
-        System.out.println("\n\n\n");
+    public void showAll(Map<String, ?> map) {
+        System.out.println("\n");
+        System.out.println("List of dishes: " + map.get("dishes"));
+        System.out.println("Name:           " + map.get("name"));
+        System.out.println("E-mail:         " + map.get("email"));
+        System.out.println("\n");
+    }
+
+
+    public void sendMail(Map<String, ?> map) {
+
+        String to = (String) map.get("email");
+        String content = "Hello, " + map.get("name") + "!\n" +
+                         "Thank You for the order. \n";
+
+
+
+
+
+
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@angryduck.com");
+        message.setTo(to);
+        message.setSubject("Your Order");
+        message.setText(content);
+
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("rozgniewanakaczka@gmail.com");
+        mailSender.setPassword("1q2w3e4r=");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+
+        mailSender.send(message);
+
 
 
     }
+
+
+
+
+
+
+
+
 
 
 }
